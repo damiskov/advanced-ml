ea7775ecbb30e5cc79029854a791ffd6819f9fc0
DavidMiles-Skov <milesskovd@gmail.com>
2025-02-12 17:44:15 +0100

    forgot to push training

---
+++ dgms/flows/utils/training.py
@@ -0,0 +1,35 @@
+from tqdm import tqdm
+
+def train(model, optimizer, data_loader, epochs, device):
+    """
+    Train a Flow model.
+
+    Parameters:
+    model: [Flow]
+       The Flow model to train.
+    optimizer: [torch.optim.Optimizer]
+         The optimizer to use for training.
+    data_loader: [torch.utils.data.DataLoader]
+            The data loader to use for training.
+    epochs: [int]
+        Number of epochs to train for.
+    device: [torch.device]
+        The device to use for training.
+    """
+    model.train()
+
+    total_steps = len(data_loader)*epochs
+    progress_bar = tqdm(range(total_steps), desc="Training")
+
+    for epoch in range(epochs):
+        data_iter = iter(data_loader)
+        for x in data_iter:
+            x = x.to(device)
+            optimizer.zero_grad()
+            loss = model.loss(x)
+            loss.backward()
+            optimizer.step()
+
+            # Update progress bar
+            progress_bar.set_postfix(loss=f"{loss.item():12.4f}", epoch=f"{epoch+1}/{epochs}")
+            progress_bar.update()
